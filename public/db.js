342697e4ad0a97cf4b55686a96aa4c208538fba1
Michael Callahan <mcallahan0@yahoo.com>
2021-08-17 16:39:55 -0400

    Implemented the request.onupgradeneeded function in db.js and set up the indexedDB

---
+++ public/db.js
@@ -0,0 +1,16 @@
+let db;
+let budgetVersion;
+
+// Create a new db request for a budget database
+const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+
+request.onupgradeneeded = function (e) {
+    const { oldVersion } = e;
+    const newVersion = e.newVersion || db.version;
+
+    db = e.target.result;
+
+    if (db.objectStoreNames.length === 0) {
+        db.createObjectStore('BudgetStore', { autoIncrement: true });
+    }
+};
