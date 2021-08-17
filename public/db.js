52e5c94f32c066f6257af481287e8a1e62852855
Michael Callahan <mcallahan0@yahoo.com>
2021-08-17 16:53:58 -0400

    Added and implemented a saveRecord function to db.js

--- public/db.js
+++ public/db.js
@@ -20,3 +20,50 @@ request.onupgradeneeded = function (e) {
 request.onerror = function (e) {
     console.log(`This error has occurred: ${e.target.errorCode}`);
 }
+
+function checkDatabase() {
+    let transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+    const store = transaction.objectStore('BudgetStore');
+
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                },
+            })
+            .then((response) => response.json())
+            .then((res) => {
+                if (res.length !== 0) {
+                    transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+                    const currentStore = transaction.objectStore('BudgetStore');
+
+                    currentStore.clear();
+                }
+            });
+        }
+    };
+}
+
+request.onsuccess = function (e) {
+    db = e.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+const saveRecord = (record) => {
+    const transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+    const store = transaction.objectStore('BudgetStore');
+
+    store.add(record);
+};
